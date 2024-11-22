d92f6a0dc59edd010a05180b5583f5cedeeaa418
Anshika Soni <anshikasoni357@gmail.com>
2024-11-22 15:55:11 +0530

    modified BS

--- Binary_Search/OneDArray/findingtarget.java
+++ Binary_Search/OneDArray/findingtarget.java
@@ -1,13 +1,14 @@
-package Binary_Search;
+package Binary_Search.OneDArray;
 
 public class findingtarget {
     public static void main(String[] args) {
         int []nums = {-1,0,3,5,9,12};
         int target = 9;
+        int mid = 0;
 
         int low = 0, right = nums.length-1;
         while(low <= right){
-            int mid = (low+right)/2;            // 3
+            mid = (low+right)/2;            // 3
             if(nums[mid] <= target){            // 5 <=9
                 if(nums[mid] == target)System.out.println(mid);
                 low = mid+1;
@@ -16,7 +17,8 @@ public class findingtarget {
                 right = mid - 1;
             }
 
-        }        
+        }     
+        // System.out.println(mid);   
 
     }
 }
