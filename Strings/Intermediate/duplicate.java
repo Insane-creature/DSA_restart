1a993d37c3efabd69bf5f5107aef402f7fe06944
Anshika Soni <anshikasoni357@gmail.com>
2024-11-27 18:10:54 +0530

    String duplicates

---
+++ Strings/Intermediate/duplicate.java
@@ -0,0 +1,23 @@
+package Strings.Intermediate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class duplicate {
+    public static void main(String[] args) {
+        String str = "programmming";
+        Map<Character, Integer> charCount = new HashMap<>();
+        
+        for (char ch : str.toCharArray()) {
+            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);    
+        }
+
+        System.out.println("Duplicate characters are:");
+        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
+
+    }
+}
