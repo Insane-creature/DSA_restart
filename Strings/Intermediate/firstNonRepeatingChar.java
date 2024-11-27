ddf459210daf5b173073eac84045973d3b26175a
Anshika Soni <anshikasoni357@gmail.com>
2024-11-28 02:38:45 +0530

    Non Repeating

---
+++ Strings/Intermediate/firstNonRepeatingChar.java
@@ -0,0 +1,26 @@
+package Strings.Intermediate;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class firstNonRepeatingChar {
+    public static void main(String[] args) {
+        String str = "swiss";
+
+        Map<Character, Integer> charCountMap = new LinkedHashMap<>();
+
+        for (char ch : str.toCharArray()) {
+            charCountMap.put(ch, charCountMap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println("First non-repeating character: " + entry.getKey());
+                return;
+            }
+        }
+
+        System.out.println("No non-repeating character found.");
+
+    }
+}
