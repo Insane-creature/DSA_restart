53978d0e833fe9f1b3487ffd4cf39ae5a81e7413
Anshika Soni <anshikasoni357@gmail.com>
2025-04-03 13:16:19 +0530

    DSA optimal

--- practice.java
+++ practice.java
@@ -23,24 +23,11 @@
 // String s = "*";
         
 
-public class practice {
+public class Practice {
     public static void main(String[] args) {
-        for (int j = 1; j <= 5; j++) {
-            for(int i = 5; i >= j; i--){
-                System.out.print("*");    
-            }
-            System.out.println();
-            
-        }
-
-        // int i = 2;
-        // System.out.println("*".repeat(i));    
-        // for(int i = 1; i <= 5; i++){
-        //     for(int j = 1; j <= i; j++){
-        //         System.out.print("*");
-        //     }    
-        //     System.out.println();
-        // }
+        int n[] = {2,7,11,15};
+        // System.out.println(n);
+        System.out.println("Hello");
     }
 }
             
